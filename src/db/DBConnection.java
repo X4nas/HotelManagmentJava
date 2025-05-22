fc8f09af14304f52b59c21a33d34e0d43dacad0d
X4nas <md.anas1028@gmail.com>
2025-05-22 22:20:37 +0530

    updated

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -1,8 +1,6 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_db";
